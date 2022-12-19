abaa19d64e912c84a791f202dae07132f2a58b40
Jason Nolfi <jason.nolfi@gmail.com>
2022-12-19 02:52:03 -0500

    feat: Style updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { Layout } from "../components/layout";
+import { fetcher } from "../utils/fetcher";
+import useSWR from "swr";
+import { Gallery } from "../components/gallery";
 
 export default function Home() {
+  const { data, error } = useSWR("/api/contentobjects", fetcher);
+
+  if (!data) return "Loading...";
+  if (error) return "Error";
+
   return (
     <Layout isHome>
-      <div>HOME</div>
+      <Gallery data={data} />
     </Layout>
   );
 }
