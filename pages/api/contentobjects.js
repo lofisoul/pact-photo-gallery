3d2bb5b523dd48d188815a0ba37b89fd60e46896
Jason Nolfi <jason.nolfi@gmail.com>
2022-12-20 13:06:44 -0500

    chore: Debug for build

--- pages/api/contentobjects.js
+++ pages/api/contentobjects.js
@@ -10,6 +10,8 @@ export default async function handler(req, res) {
 		'utf8',
 	);
 
+	console.log(fileContents);
+
 	const parseObj = JSON.parse(fileContents);
 	const imgData = parseObj.data.results;
 	//Return the content of the data file in json format
