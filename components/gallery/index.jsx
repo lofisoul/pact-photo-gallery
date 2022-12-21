ac015eef427d665d3749036f741b5c38363f0f0e
Jason Nolfi <jason.nolfi@gmail.com>
2022-12-21 07:51:11 -0500

    chore: Minor CSS fix

--- components/gallery/index.jsx
+++ components/gallery/index.jsx
@@ -5,17 +5,20 @@ export function Gallery({data, title}) {
 	return (
 		<div className={styles.galleryWrap}>
 		<h1>{title}</h1>
-		<section className={styles.gallery}>
-			{data.length === 0
+		{data.length === 0
 				? `We couldn't find any images for that album.`
-				: data.map(image => (
+		
+			
+				: <section className={styles.gallery}>{data.map(image => (
 						<div className={styles.imageItem} key={image.id}>
 							<a href={`/images/${image.id}`} key={image.id}>
 								<GalleryImage image={image} />
 							</a>
 						</div>
 				  ))}
-		</section>
+				  </section>
+				  }
+		
 		</div>
 	);
 }
